a5eea2393f8afb4a2a8ea7c457cb2ab55dfa615d
shiv <myselfsp22gmail.com>
2025-05-03 12:48:28 +0530

    complete getTodo

--- controllers/getTodo.js
+++ controllers/getTodo.js
@@ -5,6 +5,24 @@ const Todo = require("../models/Todo");
 // define rote controller
 exports.getTodo = async (req, res) => {
   try {
-    // fetch all todo item 
-  } catch (err) {}
+    // fetch all todo item
+    const todos = await Todo.find({});
+    
+    // response
+    res.status(200)
+    .json({
+        success:true,
+        data:todos,
+        message:"Entire Todo Data is fetch",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500)
+    .json({
+        success:true,
+        error:err.message,
+        message:"Server Error"
+        
+    });
+  }
 };
